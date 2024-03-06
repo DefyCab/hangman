48fd1643849e9d68dacace2b6023275e3aac4a42
defycab <love.bellinder@gmail.com>
2024-03-06 23:48:52 +0100

    add style in form of css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react"
 import words from "../wordList.json"
+// import "./App.css"
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
 
-  return <h1>Hello World!</h1> 
-   
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return (
+    <div className="container">
+      <div className="gameResult">Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+    </div>
+  )
 }
 
 export default App
