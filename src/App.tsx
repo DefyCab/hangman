afe3fd91bcdeef5d409daf66e39b52585660f5fa
defycab <love.bellinder@gmail.com>
2024-03-06 23:29:28 +0100

    choose random wordtoguess

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
+import words from "../wordList.json"
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test")
- 
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
   return <h1>Hello World!</h1> 
    
 }
