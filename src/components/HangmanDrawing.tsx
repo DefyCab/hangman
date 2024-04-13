a4e488543003ab33c66a70a1f4ad031a8042eae7
defycab <love.bellinder@gmail.com>
2024-04-13 07:23:40 +0200

    head element

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,5 +1,10 @@
+const HEAD = (
+    <div id="HEAD"></div>
+)
+
 export function HangmanDrawing() {
     return <div className="hangman-drawing">
+        {HEAD}
         <div id="smallVerticalBar"></div>
         <div id="horizontalBar"></div>
         <div id="firstUpright"></div>
