a2f7e36bbc107ba896f0736dc978130e9733aaf7
defycab <love.bellinder@gmail.com>
2024-04-13 07:08:59 +0200

    scaffolding drawing complete

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,5 +1,7 @@
 export function HangmanDrawing() {
     return <div className="hangman-drawing">
+        <div id="smallVerticalBar"></div>
+        <div id="horizontalBar"></div>
         <div id="firstUpright"></div>
         <div id="bottomBar"></div>
     </div>
