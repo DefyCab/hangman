1f1a060e0213c380f87a57076a44a085ab22dab0
defycab <love.bellinder@gmail.com>
2024-04-14 01:25:26 +0200

    right arm

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -6,10 +6,15 @@ const BODY = (
     <div id="BODY"></div>
 )
 
+const RIGHT_ARM = (
+    <div id="RIGHT_ARM"></div>
+)
+
 export function HangmanDrawing() {
     return <div className="hangman-drawing">
         {HEAD}
         {BODY}
+        {RIGHT_ARM}
         <div id="smallVerticalBar"></div>
         <div id="horizontalBar"></div>
         <div id="firstUpright"></div>
